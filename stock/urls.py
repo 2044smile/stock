c217d41dccb4f7c67da0e012868f85e68d4b3fb3
2044smile <2044smile@naver.com>
2022-11-27 13:52:05 +0900

    feat: test views

--- stock/urls.py
+++ stock/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import index
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('stock/', include("stock.urls"))
+    path('', index),
 ]
