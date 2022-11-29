e358dc5eaf07cd2e286d8eb07db97778ecdfead8
2044smile <2044smile@naver.com>
2022-11-29 14:20:34 +0900

    feat: add urls telegram

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django_telethon.urls import django_telethon_urls
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stock/', include("stock.urls"))
+    path('stock/', include("stock.urls")),
+    path('telegram/', django_telethon_urls()),
 ]
