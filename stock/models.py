3535d975a2df5f9a78e5c0c866f5cbd7d2d0bc43
2044smile <2044smile@naver.com>
2022-12-03 13:47:49 +0900

    feat: primary key

--- stock/models.py
+++ stock/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Stock(models.Model):
-    channel_id = models.IntegerField()
+    channel_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=50)  # 가치투자클럽
     username = models.CharField(max_length=50)  # corevalue
     date = models.DateTimeField()
