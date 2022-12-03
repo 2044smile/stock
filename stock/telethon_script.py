1f3c6cf8f31ad1eec866f29bf798091d8f370746
2044smile <2044smile@naver.com>
2022-12-03 15:12:19 +0900

    feat: the telegram client connection is not use django.core.exceptions.SynchronousOnlyOperation: You cannot call this from an async context - use a thread or sync_to_async. Task was destroyed but it is pending! task: <Task pending name='Task-3' coro=<Connection._send_loop() running at /Users/cslee/vscode/stock/venv/lib/python3.10/site-packages/telethon/network/connection/connection.py:311> wait_for=<Future pending cb=[Task.task_wakeup()]>> Task was destroyed but it is pending! task: <Task pending name='Task-4' coro=<Connection._recv_loop() running at /Users/cslee/vscode/stock/venv/lib/python3.10/site-packages/telethon/network/connection/connection.py:329> wait_for=<Future pending cb=[Task.task_wakeup()]>>

--- stock/telethon_script.py
+++ stock/telethon_script.py
@@ -1,10 +1,16 @@
 import os
+import sys
+sys.path.append('.')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+import django
+django.setup()
 
 # from telethon.sync import TelegramClient
 
 
 # api_id = os.getenv('TELETHON_API_ID')
-# api_hash = os.getenv('TELETHON_API_ID')
+# api_hash = os.getenv('TELETHON_API_HASH')
 # channel_username1 = 'corevalue'  # 가치투자클럽
 # channel_username2 = 'FastStockNews'  # 주식 급등일보, 급동테마 대장주 탐색기
 
@@ -22,14 +28,14 @@ from telethon.errors import SessionPasswordNeededError
 
 # Use your own values from my.telegram.org
 API_ID = os.getenv('TELETHON_API_ID')
-API_HASH = os.getenv('TELETHON_API_ID')
+API_HASH = os.getenv('TELETHON_API_HASH')
 
 app, is_created = App.objects.update_or_create(
     api_id=API_ID,
     api_hash=API_HASH
 )
-cs = ClientSession.objects.update_or_create(
-    name='default',
+cs = ClientSession.objects.get(
+    name="default",
 )
 telegram_client = TelegramClient(DjangoSession(client_session=cs), app.api_id, app.api_hash)
 telegram_client.connect()
@@ -42,4 +48,4 @@ if not telegram_client.is_user_authorized():
         telegram_client.sign_in(phone, code)
     except SessionPasswordNeededError:
         password = input('Enter your password: ')
-        telegram_client.sign_in(password=password)    
+        telegram_client.sign_in(password=password)
--- stock/urls.py
+++ stock/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import index
+from .views import index, telethon
 
 urlpatterns = [
     path('', index),
+    path('telethon/', telethon)
 ]
