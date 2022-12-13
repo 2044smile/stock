2bd48b54c3ecdb74b33d397a753afe4bf724ea88
2044smile <2044smile@naver.com>
2022-12-13 18:41:39 +0900

    refactor: clear

--- stock/telethon_script.py
+++ stock/telethon_script.py
@@ -6,46 +6,48 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 import django
 django.setup()
 
-# from telethon.sync import TelegramClient
+from telethon.sync import TelegramClient
 
 
-# api_id = os.getenv('TELETHON_API_ID')
-# api_hash = os.getenv('TELETHON_API_HASH')
+api_id = os.getenv('TELETHON_API_ID')
+api_hash = os.getenv('TELETHON_API_HASH')
+channel_list = ['corevalue', 'FastStockNews']
 # channel_username1 = 'corevalue'  # 가치투자클럽
 # channel_username2 = 'FastStockNews'  # 주식 급등일보, 급동테마 대장주 탐색기
 
-# with TelegramClient('Tim', api_id, api_hash) as client:
-#     # client.send_message('me', "Hello, myself!")
-#     channel_entity = client.get_entity(channel_username1)
-#     print(channel_entity)
+with TelegramClient('Tim', api_id, api_hash) as client:
+    # client.send_message('me', "Hello, myself!")
+    for cl in channel_list:
+        print(cl)
+        channel_entity = client.get_entity(cl)
+        print(channel_entity)
+        for message in client.get_messages(cl, limit=10):
+            print(message.message)
+# from telethon.sync import TelegramClient
+# from django_telethon.sessions import DjangoSession
+# from django_telethon.models import App, ClientSession
+# from telethon.errors import SessionPasswordNeededError
 
-#     for message in client.get_messages(channel_username1, limit=10):
-#         print(message.message)
-from telethon.sync import TelegramClient
-from django_telethon.sessions import DjangoSession
-from django_telethon.models import App, ClientSession
-from telethon.errors import SessionPasswordNeededError
+# # Use your own values from my.telegram.org
+# API_ID = os.getenv('TELETHON_API_ID')
+# API_HASH = os.getenv('TELETHON_API_HASH')
 
-# Use your own values from my.telegram.org
-API_ID = os.getenv('TELETHON_API_ID')
-API_HASH = os.getenv('TELETHON_API_HASH')
+# app, is_created = App.objects.update_or_create(
+#     api_id=API_ID,
+#     api_hash=API_HASH
+# )
+# cs = ClientSession.objects.get(
+#     name="default",
+# )
+# telegram_client = TelegramClient(DjangoSession(client_session=cs), app.api_id, app.api_hash)
+# telegram_client.connect()
 
-app, is_created = App.objects.update_or_create(
-    api_id=API_ID,
-    api_hash=API_HASH
-)
-cs = ClientSession.objects.get(
-    name="default",
-)
-telegram_client = TelegramClient(DjangoSession(client_session=cs), app.api_id, app.api_hash)
-telegram_client.connect()
-
-if not telegram_client.is_user_authorized():
-    phone = input('Enter your phone number: ')
-    telegram_client.send_code_request(phone)
-    code = input('Enter the code you received: ')
-    try:
-        telegram_client.sign_in(phone, code)
-    except SessionPasswordNeededError:
-        password = input('Enter your password: ')
-        telegram_client.sign_in(password=password)
+# if not telegram_client.is_user_authorized():
+#     phone = input('Enter your phone number: ')
+#     telegram_client.send_code_request(phone)
+#     code = input('Enter the code you received: ')
+#     try:
+#         telegram_client.sign_in(phone, code)
+#     except SessionPasswordNeededError:
+#         password = input('Enter your password: ')
+#         telegram_client.sign_in(password=password)
--- stock/views.py
+++ stock/views.py
@@ -11,60 +11,60 @@ from telethon.errors import SessionPasswordNeededError
 
 def index(request):
     return HttpResponse("Hello World")
-# sys.path.append('.')
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+sys.path.append('.')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-# import django
-# django.setup()
+import django
+django.setup()
 
-# from telethon.sync import TelegramClient
+from telethon.sync import TelegramClient
 
 
-# api_id = os.getenv('TELETHON_API_ID')
-# api_hash = os.getenv('TELETHON_API_HASH')
-# channel_username1 = 'corevalue'  # 가치투자클럽
-# channel_username2 = 'FastStockNews'  # 주식 급등일보, 급동테마 대장주 탐색기
+api_id = os.getenv('TELETHON_API_ID')
+api_hash = os.getenv('TELETHON_API_HASH')
+channel_username1 = 'corevalue'  # 가치투자클럽
+channel_username2 = 'FastStockNews'  # 주식 급등일보, 급동테마 대장주 탐색기
 
-# with TelegramClient('Tim', api_id, api_hash) as client:
-#     # client.send_message('me', "Hello, myself!")
-#     channel_entity = client.get_entity(channel_username1)
-#     print(channel_entity)
+with TelegramClient('Tim', api_id, api_hash) as client:
+    # client.send_message('me', "Hello, myself!")
+    channel_entity = client.get_entity(channel_username1)
+    print(channel_entity)
 
-#     for message in client.get_messages(channel_username1, limit=10):
-#         print(message.message)
+    for message in client.get_messages(channel_username1, limit=10):
+        print(message.message)
 
 # Use your own values from my.telegram.org
-API_ID = os.getenv('TELETHON_API_ID')
-API_HASH = os.getenv('TELETHON_API_HASH')
+# API_ID = os.getenv('TELETHON_API_ID')
+# API_HASH = os.getenv('TELETHON_API_HASH')
 
-def telethon(request):
-    if request.method == "POST":
-        app, is_created = App.objects.update_or_create(
-            api_id=API_ID,
-            api_hash=API_HASH
-        )
-        cs = ClientSession.objects.get(
-            name="default",
-        )
-        target_user = "default"
-        telegram_client = TelegramClient(DjangoSession(client_session=cs), app.api_id, app.api_hash)
-        telegram_client.connect()
+# def telethon(request):
+#     if request.method == "POST":
+#         app, is_created = App.objects.update_or_create(
+#             api_id=API_ID,
+#             api_hash=API_HASH
+#         )
+#         cs = ClientSession.objects.get(
+#             name="default",
+#         )
+#         target_user = "default"
+#         telegram_client = TelegramClient(DjangoSession(client_session=cs), app.api_id, app.api_hash)
+#         telegram_client.connect()
 
-        if not telegram_client.is_user_authorized():
-            phone = input('Enter your phone number: ')
-            telegram_client.send_code_request(phone)
-            code = input('Enter the code you received: ')
-            try:
-                telegram_client.sign_in(phone, code)
-            except SessionPasswordNeededError:
-                password = input('Enter your password: ')
-                telegram_client.sign_in(password=password)
+#         if not telegram_client.is_user_authorized():
+#             phone = input('Enter your phone number: ')
+#             telegram_client.send_code_request(phone)
+#             code = input('Enter the code you received: ')
+#             try:
+#                 telegram_client.sign_in(phone, code)
+#             except SessionPasswordNeededError:
+#                 password = input('Enter your password: ')
+#                 telegram_client.sign_in(password=password)
 
-        async def send():
-            try:
-                await telegram_client.send_message(f'@{target_user}', 'Hello from django!')
-            except ValueError:
-                print(f'Sorry no {target_user} user was found')
+#         async def send():
+#             try:
+#                 await telegram_client.send_message(f'@{target_user}', 'Hello from django!')
+#             except ValueError:
+#                 print(f'Sorry no {target_user} user was found')
 
-        with telegram_client:
-            telegram_client.loop.run_until_complete(send())
+#         with telegram_client:
+#             telegram_client.loop.run_until_complete(send())
