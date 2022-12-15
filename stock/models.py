bcf13af4a8619704e4210425fba9cadf4a1d8e35
2044smile <2044smile@naver.com>
2022-12-15 14:54:22 +0900

    refactor: add Meta and __str__

--- stock/models.py
+++ stock/models.py
@@ -12,11 +12,27 @@ class Channel(BaseModel):
     channel_id = models.IntegerField(default=0)
     name = models.CharField(max_length=32)
 
+    class Meta:
+        verbose_name = 'Channel'
+        verbose_name_plural = 'Channels'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"Channel-{self.channel_id}-{self.name}"
+
 class Stock(BaseModel):
     channel = models.ForeignKey("Channel", related_name="channel", on_delete=models.CASCADE, db_column="channel_id")
 
     title = models.CharField(max_length=255)  # 국내 대체육 시장 200억 ...
-    description = models.CharField(max_length=255)  # 상세 설명
-    site_name = models.CharField(max_length=16)  # 연론사 뷰
+    description = models.TextField()  # 상세 설명
+    site_name = models.CharField(max_length=32)  # 연론사 뷰
     url = models.URLField(max_length=255)  # 뉴스 링크
     date = models.DateTimeField()  # 작성 날짜
+
+    class Meta:
+        verbose_name = 'Stock'
+        verbose_name_plural = 'Stocks'
+        ordering = ['date']
+
+    def __str__(self):
+        return f"Stock-{self.channel}-{self.title}"
