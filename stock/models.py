9d4d0c74c8c23a00eebdf6e1ddc3b41bd8e94d73
2044smile <2044smile@naver.com>
2022-12-15 14:29:26 +0900

    refactor: sequence models

--- stock/models.py
+++ stock/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
 
-class Stock(models.Model):
-    channel_id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=50)  # 가치투자클럽
-    username = models.CharField(max_length=50)  # corevalue
-    date = models.DateTimeField()
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Channel(BaseModel):
+    channel_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=32)
+
+class Stock(BaseModel):
+    channel = models.ForeignKey("Channel", related_name="channel", on_delete=models.CASCADE, db_column="channel_id")
+
+    title = models.CharField(max_length=255)  # 국내 대체육 시장 200억 ...
+    description = models.CharField(max_length=255)  # 상세 설명
+    site_name = models.CharField(max_length=16)  # 연론사 뷰
+    url = models.URLField(max_length=255)  # 뉴스 링크
+    date = models.DateTimeField()  # 작성 날짜
