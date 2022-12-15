90a23c5e5728469acde94f7925517634ff8cc845
2044smile <2044smile@naver.com>
2022-12-15 14:54:01 +0900

    feat: create admin source

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,11 @@
+from stock.models import Channel, Stock
+
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ['channel_id', 'name']
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['channel', 'site_name', 'title', 'date']
