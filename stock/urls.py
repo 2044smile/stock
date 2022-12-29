94577587fdca75e03872f29fc86cd04c74504718
2044smile <2044smile@naver.com>
2022-12-29 17:29:46 +0900

    feat: views, urls delete telethon logic

--- stock/urls.py
+++ stock/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import index, telethon
+from .views import index
 
 urlpatterns = [
     path('', index),
-    path('telethon/', telethon)
 ]
