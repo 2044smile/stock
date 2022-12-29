dd795a3faa00387a10bd5b9007c19d9a3a5c8eda
2044smile <2044smile@naver.com>
2022-12-29 17:34:04 +0900

    refactor: minor

--- stock/views.py
+++ stock/views.py
@@ -2,12 +2,6 @@ import asyncio
 import os
 
 from django.http.response import HttpResponse
-from telethon.sync import TelegramClient
-from django_telethon.sessions import DjangoSession
-from django_telethon.models import App, ClientSession
-from telethon.errors import SessionPasswordNeededError
-
-from stock.models import Channel, Stock
 
 
 def index(request):
