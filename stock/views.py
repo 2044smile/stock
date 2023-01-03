fa2dadff263516c46fd6cde15350d8a4bad20744
2044smile <2044smile@naver.com>
2023-01-03 22:16:45 +0900

    refactor: ListView

--- stock/views.py
+++ stock/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.views.generic import ListView
 
 from .models import Stock
 
 
-def index(request):
-    stock_list = Stock.objects.all()
-    return render(request, 'index.html', {'stock_list': stock_list})
+class IndexListView(ListView):
+    paginate_by = 10
+    model = Stock
+    
