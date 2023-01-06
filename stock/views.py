b5aa319e76803ce2b39e109a95cbb5bcc4542e5a
2044smile <2044smile@naver.com>
2023-01-06 14:26:08 +0900

    feat: StockIndexView

--- stock/views.py
+++ stock/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from .models import Stock
 
 
+class StockIndexView(TemplateView):
+    template_name = 'base.html'
 class StockListView(ListView):
     model = Stock
     paginate_by = 10
