06bc1f6d76f2b61d45d0c57e2620152385726e72
2044smile <2044smile@naver.com>
2023-01-09 14:53:19 +0900

    feat: DetailView

--- stock/urls.py
+++ stock/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import StockListView, StockIndexView
+from .views import StockListView, StockIndexView, StockDetailView
 
 urlpatterns = [
     path('', StockIndexView.as_view(), name='index'),
     path('list/', StockListView.as_view(), name='stocks'),
+    path('detail/<int:pk>/', StockDetailView.as_view(), name='stock_detail'),
 ]
