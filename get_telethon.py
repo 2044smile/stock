53e044c848ebcb280c7dbc8f9bd53acedbd7edec
2044smile <2044smile@naver.com>
2023-01-11 19:24:23 +0900

    refactor: there are many difficult problems

--- get_telethon.py
+++ get_telethon.py
@@ -82,37 +82,36 @@ def get_telethon():
                             success_news_link.append(message.message)
                             print(success_news_link)
                     except AttributeError:
-                        print('False')
-                        """
-                        시간외특징주 와 같이 링크나 추가 설명이 없을 경우
+                        print('There are many difficult problems')
+                        # print('False')
+                        # """
+                        # 시간외특징주 와 같이 링크나 추가 설명이 없을 경우
 
-                        Ex by msg_list)
-                        12월 15일 시간외특징주\n  [0]
-                        https://cafe.naver.com/stockhunters/99194\n Posts that are only open to members [1]
-                        12월 15일 52주 신고가 및 급등락주\n  [2]
-                        https://cafe.naver.com/stockhunters/99195' You can see it even if you are not a member  [3]
-                        """
-                        msg_list = message.message.split('\n')
-                        if len(msg_list) != 3:
-                            print(msg_list)
-                            break
+                        # Ex by msg_list)
+                        # 12월 15일 시간외특징주\n  [0]
+                        # https://cafe.naver.com/stockhunters/99194\n Posts that are only open to members [1]
+                        # 12월 15일 52주 신고가 및 급등락주\n  [2]
+                        # https://cafe.naver.com/stockhunters/99195' You can see it even if you are not a member  [3]
+                        # """
+                        # msg_list = message.message.split('\n')
+                        # if len(msg_list) != 3:
+                        #     print(msg_list)
+                        #     break
 
-                        title = msg_list[0]
-                        description = msg_list[2]
-                        url = msg_list[3]
+                        # title = msg_list[0]
+                        # description = msg_list[2]
+                        # url = msg_list[3]
 
-                        Stock.objects.update_or_create(
-                            channel=obj,
-                            title=title,
-                            description=description,
-                            site_name="시간외 특징주",
-                            url=url,  # Naver Cafe must sign up in the case of 'msg_list[1]'
-                            date=message.date
-                        )
-                        fail_news_link.append(message.message)
-                        print(fail_news_link)
-                    except Stock.DoesNotExist:
-                        print('yes')
+                        # Stock.objects.update_or_create(
+                        #     channel=obj,
+                        #     title=title,
+                        #     description=description,
+                        #     site_name="시간외 특징주",
+                        #     url=url,  # Naver Cafe must sign up in the case of 'msg_list[1]'
+                        #     date=message.date
+                        # )
+                        # fail_news_link.append(message.message)
+                        # print(fail_news_link)
         except ValueError:
             print(f'Sorry no {target_user} user was found')
 
