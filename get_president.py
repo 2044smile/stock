c6d99de7b0b4f40b1ed598297178d52411b7ad95
2044smile <2044smile@naver.com>
2023-01-27 14:18:43 +0900

    feat: a href get link

--- get_president.py
+++ get_president.py
@@ -6,7 +6,10 @@ from bs4 import BeautifulSoup
 class PresidentNewsroomCrawling:
     def __init__(self):
         print('__init__')
-
+        
+        self.link_url = []
+        
+        self.president_url = requests.get(url='https://www.president.go.kr')
         self.president_newsroom_url = requests.get(url='https://www.president.go.kr/newsroom')
         self.president_newsroom_fact_url = requests.get(url='https://www.president.go.kr/newsroom/fact/')
         self.president_newsroom_briefing_url = requests.get(url='https://www.president.go.kr/newsroom/briefing/')
@@ -21,5 +24,12 @@ class PresidentNewsroomCrawling:
 
         soup = BeautifulSoup(html.text, 'html.parser')
         bs = soup.select(
-            "body > div.container > div > section.briefing"
+            "body > div.container > div > section.fact > div > div > a"
         )
+        for b in bs:
+            self.link_url.append(self.president_url.url.rstrip('/') + b.get('href'))
+        
+        print(self.link_url)
+        
+p = PresidentNewsroomCrawling()
+p.crawler('fact')
