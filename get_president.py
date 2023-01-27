430f1dd6152c293fd4111e75ef62b12fc7ccec42
2044smile <2044smile@naver.com>
2023-01-27 13:21:31 +0900

    feat: get_president

--- get_president.py
+++ get_president.py
@@ -1,1 +1,22 @@
+import os
+
 from bs4 import BeautifulSoup
+
+
+class PresidentNewsroomCrawling:
+    def __init__(self):
+        print('__init__')
+
+        self.president_newsroom_url = 'https://www.president.go.kr/newsroom'
+        self.president_newsroom_fact_url = 'https://www.president.go.kr/newsroom/fact/'
+        self.president_newsroom_briefing_url = 'https://www.president.go.kr/newsroom/briefing/'
+        self.headers = {"Content-Type": "application/json"}
+        
+
+    def crawler(self, target):
+        if target == 'fact':
+            html = self.president_newsroom_fact_url
+        elif target == 'briefing':
+            html = self.president_newsroom_briefing_url
+
+        soup = BeautifulSoup(html.text, 'html.parser')
