e237f7d7ceb71f3e4b6d17659089a4303e77fb09
2044smile <2044smile@naver.com>
2023-01-30 20:41:47 +0900

    feat: this correct?

--- get_president.py
+++ get_president.py
@@ -28,36 +28,35 @@ class PresidentNewsroomCrawling:
             bs = soup.select(
                 "body > div.container > div > section.fact > div > div > a"
             )
-            for b in bs:  # newsroom/fact/ 왜 여기에 for 문이 있어야 할까?
+            for b in bs:
                 self.link_url.append(self.president_url.url.rstrip('/') + b.get('href'))
-            
-            for url in self.link_url:  # link_url to url 로 들어가서 한번 더 크롤링 진행
-                html = requests.get(url=url)
-                soup = BeautifulSoup(html.text, 'html.parser')
-                bs = soup.select(
-                    "body > div.container > div > section.view_area"
-                )
 
-                for b in bs:
-                    text = b.get_text().replace("\n", "").strip("복사확인").replace("\xa0", "")  # 필요없는 문자 제거
-                    
-                    match = re.search(r'\d{4}.\d{2}.\d{2}', text)  # text 에서 날짜 까내기
-                    date = datetime.strptime(match.group(), '%Y.%m.%d')  # match.group() 2023.01.27 to datetime 으로 만들기
-                    remove_dates = re.sub(r'\d{4}.\d{2}.\d{2}', '', text)  # text 에서 날짜 제거
-
-                    get_info = remove_dates.split('→')
-
-                    get_title = get_info[0]
-                    get_description = get_info[1].split("목록")[0]
-                    get_link = get_info[1].split("목록")[0].split("링크")[0]
-
-                    PresidentFact.objects.update_or_create(
-                        title=get_title,
-                        description=get_description,
-                        link=get_link,
-                        date=date
+                for url in self.link_url:                
+                    html = requests.get(url=url)
+                    soup = BeautifulSoup(html.text, 'html.parser')
+                    bs = soup.select(
+                        "body > div.container > div > section.view_area"
                     )
-            
+
+                    for b in bs:
+                        text = b.get_text().replace("\n", "").strip("복사확인").replace("\xa0", "")  # 필요없는 문자 제거
+                        
+                        match = re.search(r'\d{4}.\d{2}.\d{2}', text)  # text 에서 날짜 까내기
+                        date = datetime.strptime(match.group(), '%Y.%m.%d')  # match.group() 2023.01.27 to datetime 으로 만들기
+                        remove_dates = re.sub(r'\d{4}.\d{2}.\d{2}', '', text)  # text 에서 날짜 제거
+
+                        get_info = remove_dates.split('→')
+
+                        get_title = get_info[0]
+                        get_description = get_info[1].split("목록")[0]
+                        get_link = get_info[1].split("목록")[0].split("링크")[0]
+
+                        PresidentFact.objects.update_or_create(
+                            title=get_title,
+                            description=get_description,
+                            link=get_link,
+                            date=date
+                        )
         elif target == 'briefing':
             html = self.president_newsroom_briefing_url
         
