66e857095443dc4eeee3347adaa5a849739d2a19
2044smile <2044smile@naver.com>
2023-01-30 15:01:41 +0900

    feat: complated creating admin page

--- stock/admin.py
+++ stock/admin.py
@@ -1,4 +1,4 @@
-from stock.models import Channel, Stock
+from stock.models import Channel, Stock, PresidentFact, PresidentBriefing
 
 from django.contrib import admin
 
@@ -9,3 +9,7 @@ class ChannelAdmin(admin.ModelAdmin):
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
     list_display = ['channel', 'site_name', 'title', 'description', 'date']
+
+@admin.register(PresidentFact)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'date']
