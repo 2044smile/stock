0f6f22bb2953ac4b6aa23f2666d4a3f66a44c915
2044smile <2044smile@naver.com>
2023-01-30 14:37:41 +0900

    refactor: remove detes, special_characters and links

--- get_president.py
+++ get_president.py
@@ -1,4 +1,6 @@
+import re
 import os, requests
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 
@@ -36,8 +38,22 @@ class PresidentNewsroomCrawling:
                 )
 
                 for b in bs:
-                    text = b.get_text().replace("\n", "").strip("복사확인")
-                    text_lst.append(text)
+                    text = b.get_text().replace("\n", "").strip("복사확인").replace("\xa0", "")
+                    
+                    # 날짜 가져오기 after, save database
+                    match = re.search(r'\d{4}.\d{2}.\d{2}', text)
+                    date = datetime.strptime(match.group(), '%Y.%m.%d')  # match.group() 2023.01.27 to datetime
+                    remove_date = re.sub(r'\d{4}.\d{2}.\d{2}', '', text)  # text 에서 날짜 제거
+                    
+                    # special characters 제거 to List
+                    remove_special_characters = remove_date.split('→')
+                    # print(remove_special_characters[0])  # title
+                    # print(remove_special_characters[1])  # description
+
+                    import pdb;
+                    pdb.set_trace()
+                    remove_links = remove_special_characters.split('https://')[1].split("링크")[0]
+                    print(remove_links) 
             
         elif target == 'briefing':
             html = self.president_newsroom_briefing_url
