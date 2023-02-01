4944faf2019046d2e6fda039997adc85b25310f8
2044smile <2044smile@naver.com>
2023-02-01 20:15:16 +0900

    :recycle: change telegram to index
    done

--- stock/urls.py
+++ stock/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from .views import StockListView, StockIndexView
 
 urlpatterns = [
-    path('telegram/', StockIndexView.as_view(), name='index'),
+    path('index/', StockIndexView.as_view(), name='index'),
     path('telegram/list/', StockListView.as_view(), name='stock_list'),
 
     # path('president/newsroom/'),
