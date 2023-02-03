164f5f2ff8bdcd0e887e4c7324a54ddeb09985ea
2044smile <2044smile@naver.com>
2023-02-03 15:44:12 +0900

    :recycle: minor and add container_name

--- config/settings.py
+++ config/settings.py
@@ -102,9 +102,9 @@ DATABASES = {
         "USER": env('POSTGRES_USERNAME'),
         "PASSWORD": env('POSTGRES_PASSWORD'),
         "HOST": "db",
+        "PORT": 5432,
         # "db" -> docker-compose 로 띄운 db(postgres)를 바라보는 것
         # "localhost" -> docker-compose 로 띄운 web(django)의 database 를 바라보는 것
-        "PORT": 5432,
     }
 }
 
