a9c8fde58cf5fad4c069f60e39b31c25673f8adf
2044smile <2044smile@naver.com>
2023-02-08 13:00:33 +0900

    :sparkles: signin, signup, signout

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('signin/', ),
+    path('signup/'),
+    path('signout/')
+]
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,9 @@ from django_telethon.urls import django_telethon_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('account/', include("account.urls")),
     path('stock/', include("stock.urls")),
+
     path('telegram/', django_telethon_urls()),
 ]
