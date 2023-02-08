0132094dab0d6704d48706e3483522d0e972e44c
2044smile <2044smile@naver.com>
2023-02-08 21:31:41 +0900

    :recycle: add accounts

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'stock',
+    'accounts',
 
     'django_telethon',
     'django_extensions',
--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('stock/', include("stock.urls")),
+    path('accounts/', include("accounts.urls")),
 
     path('telegram/', django_telethon_urls()),
 ]
