1125ec63f1151f46ef9b2e8462b00cb486d1f880
2044smile <2044smile@naver.com>
2023-02-08 19:31:38 +0900

    :recycle: restart accounts

--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,6 @@ from django_telethon.urls import django_telethon_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('accounts/', include("dj_rest_auth.urls")),
-    path('accounts/', include("allauth.urls")),
-    # path('accounts/', include("accounts.urls")),
     path('stock/', include("stock.urls")),
 
     path('telegram/', django_telethon_urls()),
