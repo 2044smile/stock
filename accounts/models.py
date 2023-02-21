9657f62628e3e61ddae0776d7634290fe561ab6e
2044smile <2044smile@naver.com>
2023-02-21 21:11:49 +0900

    :sparkles: make it email, kakao_nickname, stock_code 1,2,3

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+from stock.models import BaseModel
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, kakao_nickname, password=None):
+        if not email:
+            raise ValueError('must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            kakao_nickname=kakao_nickname,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    email = models.EmailField(
+        verbose_name='Email address',
+        max_length=255,
+        unique=True
+    )
+    kakao_nickname = models.CharField(
+        verbose_name='Kakao Nickname',
+        max_length=32
+    )
+    stock_code_1 = models.CharField(
+        verbose_name='stock_code_1',
+        max_length=8
+    )
+    stock_code_2 = models.CharField(
+        verbose_name='stock_code_2',
+        max_length=8
+    )
+    stock_code_3 = models.CharField(
+        verbose_name='stock_code_3',
+        max_length=8
+    )
+    is_active = models.BooleanField(
+        verbose_name='Is active',
+        default=True
+    )
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
