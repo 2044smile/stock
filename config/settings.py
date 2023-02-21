3e48497c0639f47881c369ebdcafc943f5d05e6d
2044smile <2044smile@naver.com>
2023-02-21 21:11:14 +0900

    :sparkles: feat: make accounts.User

--- config/settings.py
+++ config/settings.py
@@ -156,3 +156,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
