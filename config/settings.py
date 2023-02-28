1cd14d7ad99c13e7e6e79d55504ea4386099b8bf
2044smile <2044smile@naver.com>
2023-02-28 15:35:03 +0900

    :recycle: LANGUAGE_CODE change us to ko

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
 TIME_ZONE = 'Asia/Seoul'
 
